df1d8d1a27c1289cdcaf0fcf19323364a660f6da
Aduwia Ransford <87871201+aduwiaransford@users.noreply.github.com>
2023-08-31 17:59:46 -0700

    commit

--- client/src/context/authContext/apiCalls.js
+++ client/src/context/authContext/apiCalls.js
@@ -4,7 +4,7 @@ import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post("https://aamusted-api.onrender.com/login", user);
+    const res = await axios.post("/login", user);
     dispatch(loginSuccess(res.data)); // res.data should include isAdmin property
     console.log(res.data);
     return res.data; // Return the response data
